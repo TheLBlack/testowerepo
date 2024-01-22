aaa373170254a9a8fcc94855042e679cb80e701f
Emil Rafalko <emil.rafalko@ringieraxelspringer.pl>
2024-01-22 19:04:12 +0100

    drugi commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void jakasMetoda() {
+
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
